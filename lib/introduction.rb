5e9f0ad0a2e8178c0e8270734164a5da83a75516
Nicole Parrish <nicoleparrish.parrish@gmail.com>
2019-05-10 00:34:51 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,4 +1,11 @@
+require "pry"
+
 def introduction(name)
-  puts "Hi, my name is #
-  {name} ."
+  puts "Hi, my name is #{name}."
+end
+
+
+
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
